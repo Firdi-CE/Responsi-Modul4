25ec6a433e2b9acb09c876d9735848b01dd19dd1
Firdi <65402012+Firdi-CE@users.noreply.github.com>
2025-11-17 16:22:45 +0000

    Projects data added

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,20 +5,27 @@ export const staticProjects = [
     title: 'Proyek Responsi Modul 4',
     description: 'Membuat aplikasi PWA portofolio menggunakan Vite, React, dan Tailwind.',
     tech: ['React', 'PWA', 'Tailwind CSS'],
-    githubUrl: 'https://github.com/firdi-ce/responsi-modul4' // Ganti dengan URL repo Anda
+    githubUrl: 'https://github.com/Firdi-CE/Responsi-Modul4' 
   },
   {
     id: 2,
     title: 'Aplikasi Resep Nusantara',
     description: 'Aplikasi web dari modul praktikum untuk menampilkan resep makanan dan minuman.',
     tech: ['React', 'CSS', 'JavaScript'],
-    githubUrl: 'https://github.com/fadlishidqi/Resep-Nusantara-Modul4' // Contoh dari PDF
+    githubUrl: 'https://github.com/fadlishidqi/Resep-Nusantara-Modul4' 
   },
   {
     id: 3,
-    title: 'Proyek Lain',
-    description: 'Deskripsi singkat proyek lain yang pernah Anda kerjakan.',
-    tech: ['HTML', 'CSS'],
-    githubUrl: 'https://github.com/'
+    title: 'Infinite Dungeon Crawler',
+    description: 'This is a classic roguelike dungeon crawler game built in Python. It uses the libtcod library to handle the display, input, and map logic.',
+    tech: ['Python', 'libtcod'],
+    githubUrl: 'https://github.com/Firdi-CE/IDC-Infinite-Dungeon-Crawler-'
+  },
+  {
+    id: 4,
+    title: 'Unicode Message Decoder',
+    description: 'a Python utility that fetches a publicly published Google Doc, parses a table of 2D coordinates and Unicode characters, and renders the resulting 2D graphic in the console',
+    tech: ['Python'],
+    githubUrl: 'https://github.com/Firdi-CE/Unicode-Secret-Message-Decoder'
   }
 ];
